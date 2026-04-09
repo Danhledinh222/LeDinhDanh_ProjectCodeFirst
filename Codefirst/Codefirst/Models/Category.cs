fdf7e82b47732f356f7dcf608a002b29988661ea
Lê Đình Danh <danhledinh2607@gmail.com>
2026-04-09 21:18:58 +0700

    Bước 3: Tạo các Model Category và Product với liên kết 1-Nhiều

---
+++ Codefirst/Codefirst/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace HomeworkCodeFirst.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ Codefirst/Codefirst/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HomeworkCodeFirst.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")] 
+        public Category Category { get; set; }
+    }
+}
